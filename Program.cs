3ae9702632d1a7730859f23db910788e25a38f06
Nathan Swetzof <nswetzof@gmail.com>
2023-08-31 20:27:33 -0700

    Progress implementing voice lookup function.

--- Program.cs
+++ Program.cs
@@ -9,11 +9,36 @@ using System.Collections.Generic;
 using System.Text.Json.Serialization;
 using Newtonsoft.Json;
 using System.Text;
+using System.Text.Json.Nodes;
+using Newtonsoft.Json.Linq;
 
 namespace HttpClientSample {
     public class Program {
         private static string API_KEY = "0591d8c076c8faeb2b9acfd16aadfffd";
         static readonly HttpClient client = new HttpClient();
+
+        static async Task<string?> GetVoiceAsync(string voiceName) {
+            client.BaseAddress = new Uri("http://api.elevenlabs.io/v1");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Add("s-api-key", Program.API_KEY);
+
+            using (HttpResponseMessage response = await client.GetAsync(new Uri($"{client.BaseAddress}/voices"))) {
+                if(response.IsSuccessStatusCode) {
+                    string? voiceString = JsonConvert.SerializeObject(await response.Content.ReadAsStringAsync());
+                    
+                    JObject responseJson = JObject.Parse(voiceString);
+
+                    var id = 
+                        from item in responseJson["voices"]
+                        where (string) (item["name"]) == voiceName
+                        select (string) item["voice_id"];
+
+                    return id.First();
+                }
+                else
+                    return "failure: " + response.ReasonPhrase;
+            }
+        }
 
         static async Task<string?> GenerateVoice(string voiceId) {
             string url = $"https://api.elevenlabs.io/v1/text-to-speech/{voiceId}/stream";
@@ -60,30 +85,32 @@ namespace HttpClientSample {
             client.DefaultRequestHeaders.Add("Accept", "application/json");
 
             // create form data handler
-            var content = new MultipartFormDataContent();
-
-            content.Add(new StringContent("Obama"), "name");
-            content.Add(new StringContent("Obama speaking in a normal tone"));
+            var content = new MultipartFormDataContent
+            {
+                { new StringContent("Obama"), "name" },
+                { new StringContent("Obama speaking in a normal tone"), "description" }
+            };
             // content.Add(new StringContent("{\"accent\":\"American\"}"));
-            var dataContent = new FormUrlEncodedContent(new[] {
-                new KeyValuePair<string, string>("accent", "American"),
-            });
-            content.Add(dataContent);
+            // var dataContent = new FormUrlEncodedContent(new[] {
+            //     new KeyValuePair<string, string>("accent", "American")
+            // });
+            // content.Add(dataContent);
             // byte[] bytes = await File.ReadAllBytesAsync(file);
-            content.Add(new ByteArrayContent(await File.ReadAllBytesAsync(file)), "sample1", "sample1.mp3");
+            content.Add(new ByteArrayContent(await File.ReadAllBytesAsync(file)), "audio/mpeg", "sample1.mp3");
 
             using(HttpResponseMessage response = await client.PostAsync(url, content)) {
                 if(response.IsSuccessStatusCode) {
                     Console.WriteLine("Success!");
                 }
                 else
-                    Console.WriteLine(response.StatusCode.ToString());
+                    Console.WriteLine(response.ReasonPhrase);
             }
 
         }
         static void Main() {
             // Console.WriteLine(GenerateVoice("21m00Tcm4TlvDq8ikWAM").GetAwaiter().GetResult());
-            UploadVoice("sample1.mp3").GetAwaiter().GetResult();
+            Console.WriteLine(GetVoiceAsync("Emma Watson").GetAwaiter().GetResult());
+            // UploadVoice("sample1.mp3").GetAwaiter().GetResult();
         }
     }
 }
